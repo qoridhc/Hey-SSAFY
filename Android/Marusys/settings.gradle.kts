5528f0812a7c6c010fb9e3c4f044c8676d261048
이재빈 <jeabin1129@naver.com>
2024-11-01 17:59:02 +0900

    [AND] Feat: hot-word-spotting

--- Android/Marusys/settings.gradle.kts
+++ Android/Marusys/settings.gradle.kts
@@ -9,13 +9,18 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-    }
+
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // TarsosDSP 라이브러리의 저장소 추가
+        maven {
+            url = uri("https://mvn.0110.be/releases")
+        }
+    }
     }
 }
 
